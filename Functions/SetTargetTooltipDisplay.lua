c9b2ae85eda76c2254d3cb88d8175ace0dd413e5
UnsafeCell <238906487+unsafecell@users.noreply.github.com>
2025-10-18 13:42:51 -0700

    Use player stats to reveal mobs lvl/health/name

--- Functions/SetTargetTooltipDisplay.lua
+++ Functions/SetTargetTooltipDisplay.lua
@@ -6,8 +6,23 @@ function SetTargetTooltipDisplay(hideTargetTooltip)
       local unit = select(2, self:GetUnit())
       if not unit then return end
 
+      if AuraUtil.FindAuraByName("Flare", unit, "HARMFUL") then return end
+      if AuraUtil.FindAuraByName("Cozy Fire", "player", "HELPFUL") then return end
+
+      local _, total_str, _, _ = UnitStat("player", 1);
+      local _, total_agi, _, _ = UnitStat("player", 2);
+      local _, total_int, _, _ = UnitStat("player", 4);
+      local lvl = UnitLevel(unit);
+
       -- Hide health bar graphic under tooltip
-      GameTooltipStatusBar:Hide()
+      if total_agi < 5 * lvl then
+        GameTooltipStatusBar:Hide()
+      end
+
+      local name = GameTooltipTextLeft1;
+      if total_int < 5 * lvl and not UnitIsPlayer(unit) and not UnitIsFriend("player", unit) then
+        name:SetText(UnitCreatureType(unit))
+      end
 
       -- Modify tooltip lines
       for i = 2, self:NumLines() do
@@ -15,8 +30,8 @@ function SetTargetTooltipDisplay(hideTargetTooltip)
         if line then
           local text = line:GetText()
           if text and not UnitIsPlayer(unit) then
-            if text:match(LEVEL) then
-              line:SetText('')
+            if total_str < 5 * lvl and text:match(LEVEL) and not UnitIsFriend("player", unit) then
+              line:SetText('??')
             end
           end
         end
